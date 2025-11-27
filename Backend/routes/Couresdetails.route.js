eb4ebf88239b8b43e2afdd00a40b6678c16d552c
ijasahamedstr <ijasahamed.str@gmail.com>
2025-11-27 15:12:05 +0300

    commit

--- Backend/routes/Couresdetails.route.js
+++ Backend/routes/Couresdetails.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import { Courescreate } from '../controller/coures.Controller.js';
 
 // Create a new router instance
 const Couressection = express.Router();
 
 
 // Create the Data Register
-Couressection.post('/', );
+Couressection.post('/', Courescreate);
 
 
 
