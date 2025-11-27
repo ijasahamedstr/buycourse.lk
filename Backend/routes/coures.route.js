a6b92403cc2672f242116ee8cbdd65c8a15842b8
ijasahamedstr <ijasahamed.str@gmail.com>
2025-11-27 13:24:09 +0300

    commit

--- Backend/routes/coures.route.js
+++ Backend/routes/coures.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { CourseCreate, CourseDelete, CourseIndex, CourseSingleDetails } from "../controller/Course.Controller.js";
+import { CourseCreate } from "../controller/Course.Controller.js";
 
 // Create a new router instance
 const CourseSection = express.Router();
@@ -7,16 +7,5 @@ const CourseSection = express.Router();
 // Create new course
 CourseSection.post("/", CourseCreate);
 
-// View all courses
-CourseSection.get("/", CourseIndex);
-
-// View single course details
-CourseSection.get("/:id", CourseSingleDetails);
-
-// Delete a course
-CourseSection.delete("/:id",CourseDelete );
-
-// // Update a course
-// CourseSection.put("/:id", CourseUpdate);
 
 export default CourseSection;
