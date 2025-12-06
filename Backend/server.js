b69d4ebaa6708eb9fd0ce0a1b2c04c21f1809fa5
ijasahamedstr <ijasahamed.str@gmail.com>
2025-12-06 17:51:45 +0300

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "https://buycourse-lk.vercel.app",
+      "https://www.buycourse.lk",
       "https://buycourse-lk-umlb.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
